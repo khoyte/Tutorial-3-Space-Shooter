df7202480030a8d98c1f45bfdcee9b42ac070eca
Kayla H <hoytekayla@gmail.com>
2020-04-03 14:36:01 -0400

    Challenge 3
    Challenge 3 for DIG 3480 on 4/3/2020

---
+++ Space Shooter Project/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        // where background starts in Unity
+        startPosition = transform.position;
+    }
+    void Update()
+    {
+        // repeats background with tile size z and controlled by scrolling speed
+        float newPosition = Mathf.Repeat (Time.time * scrollSpeed, tileSizeZ);
+        // moving background with scroll speed
+        transform.position = startPosition + Vector3.forward * newPosition;
+    }
+}
---
+++ Space Shooter Project/Assets/Scripts/EvasiveManeuver.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvasiveManeuver : MonoBehaviour
+{
+    public float dodge;
+    public float smoothing;
+    public float tilt;
+    public Vector2 startWait;
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+    public Boundary boundary;
+
+    private float currentSpeed;
+    private float targetManeuver;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = rb.velocity.z;
+        StartCoroutine (Evade());
+    }
+
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds (Random.Range (startWait.x, startWait.y));
+
+        while (true)
+        {
+            // enemy movement from a range 1 to dodge, the sign will multiply with the transform position (negative number). Controls how the dodge works
+            // (will always dodge inwards, keeping it on screen)
+            targetManeuver = Random.Range (1, dodge) * -Mathf.Sign (transform.position.x);
+            yield return new WaitForSeconds (Random.Range (maneuverTime.x, maneuverTime.y));
+            targetManeuver = 0;
+            yield return new WaitForSeconds (Random.Range (maneuverWait.x, maneuverWait.y));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float newManeuver = Mathf.MoveTowards (rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+        rb.velocity = new Vector3 (newManeuver, 0.0f, currentSpeed);
+        rb.position = new Vector3
+            (
+                Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+            );
+
+        rb.rotation = Quaternion.Euler (0, 0, rb.velocity.x * -tilt);
+    }
+}
